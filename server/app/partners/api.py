ea9b5932970b3256db3bd6845fb867e27b1e5662
Muinde Geofrey <muindegeofrey@gmail.com>
2022-07-31 18:12:01 +0300

    access control

--- server/app/partners/api.py
+++ server/app/partners/api.py
@@ -14,10 +14,10 @@ router = APIRouter(prefix="/partner")
 
 @router.post("/", response_model=PartnerSerializer)
 def create_partner(
-        db: Session = Depends(deps.get_db),
-        *,
-        obj_in: PartnerCreateSerializer,
-        admin_member: PartnerMember = Depends(get_super_admin_member),
+    db: Session = Depends(deps.get_db),
+    *,
+    obj_in: PartnerCreateSerializer,
+    admin_member: PartnerMember = Depends(get_super_admin_member),
 ) -> PartnerSerializer:
     try:
         return partner_dao.create(db, obj_in=obj_in)
@@ -31,21 +31,21 @@ def create_partner(
 
 @router.get("/", response_model=Page[PartnerSerializer])
 def get_partners(
-        db: Session = Depends(deps.get_db),
-        pagination: PaginationQueryParams = Depends(),
-        admin_member: PartnerMember = Depends(get_super_admin_member),
-        _=Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db),
+    pagination: PaginationQueryParams = Depends(),
+    admin_member: PartnerMember = Depends(get_super_admin_member),
+    _=Depends(deps.get_current_user),
 ) -> Page[PartnerSerializer]:
     return partner_dao.get_multi_paginated(db, params=pagination)
 
 
 @router.get("/{bp_id}/", response_model=PartnerSerializer)
 def get_by_id(
-        db: Session = Depends(deps.get_db),
-        *,
-        bp_id: str,
-        admin_member: PartnerMember = Depends(get_super_admin_member),
-        _=Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db),
+    *,
+    bp_id: str,
+    admin_member: PartnerMember = Depends(get_super_admin_member),
+    _=Depends(deps.get_current_user),
 ) -> PartnerSerializer:
     return partner_dao.get_not_none(
         db, id=bp_id
